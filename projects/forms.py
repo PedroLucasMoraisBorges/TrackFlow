0f9015fc7d6fdad110f7f3feb0ffc2bde6e62fda
Pedro Lucas <pedrolucasmoraisborges@gmail.com>
2025-03-17 14:18:37 -0300

    feat(returning_milestones)

--- projects/forms.py
+++ projects/forms.py
@@ -63,3 +63,40 @@ class RegisterUserProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['name', 'description'] 
+
+class EditProjectForm(forms.ModelForm):
+    name = forms.CharField(
+        required = True,
+        label = 'Nome',
+        widget = forms.TextInput(attrs={
+            'placeholder' : 'Nome do Projeto',
+            'class' : ''
+        })
+    )
+
+    description = forms.CharField(
+        required=True,
+        label='Descrição',
+        widget=forms.Textarea(attrs={
+            'placeholder' : 'Descrição do projeto',
+            'class' : ''
+        })
+    )
+
+    def save(self, commit=True, instance=None):
+        """Salva apenas os campos preenchidos no formulário."""
+        if instance is None:
+            instance = super().save(commit=False)
+        else:
+            for field in self.cleaned_data:
+                value = self.cleaned_data[field]
+                if value:
+                    setattr(instance, field, value)
+
+        if commit:
+            instance.save()
+        return instance
+
+    class Meta:
+        model = Project
+        fields = ['name', 'description'] 
--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from .models import *
 from .forms  import  * 
+from milestones.models import *
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -49,3 +50,44 @@ class RegisterProject(APIView):
                 'errors' : getErrors([form])
             }, status= status.HTTP_400_BAD_REQUEST
         )
+    
+class ViewProject(View):
+    def get(self, request, id):
+        project = Project.objects.get(id=id)
+        edit_project_form = EditProjectForm(instance=project)
+
+        project_milestones = Milestone.objects.filter(fk_project = project)
+        returning_milestones = []
+
+        for mls in project_milestones:
+            stages = mls.stages.order_by('dt_creation')
+            returning_stages = []
+            files = mls.files.all()
+
+            for stg in stages:
+                stg_files = stg.files.all()
+                stg_stgs = stg.stages.all()
+
+                returning_stages.append(
+                    {   
+                        'info' : stg,
+                        'files' : stg_files,
+                        'sub_stages' : stg_stgs
+                    }
+                )
+
+            returning_milestones.append(
+                {
+                'info' : mls,
+                'files' : files,
+                'stages' : returning_stages
+                }
+            )
+        
+        context = {
+            'projetc' : project,
+            'edit_project_form' : edit_project_form,
+            'milestones' : returning_milestones
+        }
+
+        return render(request, 'manager/viewProject.html', context)
