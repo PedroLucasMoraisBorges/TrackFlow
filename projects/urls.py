0a91b9744c2f6eb77a456c6c2e4f365e14ebff88
Pedro Lucas <pedrolucasmoraisborges@gmail.com>
2025-04-15 19:40:42 -0300

    feat(integration_with_gemini)

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('viewProject/<str:id>', ViewProject.as_view(), name='view_project'),
     path('myProjects/', Projects.as_view(), name='my_projects'),
+    path('createProjectWithAi/', CreateProjectWithAi.as_view(), name='generate_project')
 ]
